8e898fe3e21fdd8cd5c62bb0bfcad194a380a188
Ductai <luuductai12@gmail.com>
2023-12-12 20:40:39 +0700

    khoi tao lai code

--- database/function.php
+++ database/function.php
@@ -1,12 +1,13 @@
 
 <?php
-    include "connect.php";
+    
     function createRow(){
+        include "connect.php";
         if(isset($_POST['submit'])){
             $email = $_POST['email'];
             $password = $_POST['password'];
             $fullName = $_POST =['fullname'];
-            $query = "INSERT INTO users(email, password, fullName) VALUE('$email','$password','$fullName')";  
+            $query = "INSERT INTO users(email, password, fullName) VALUES('$email','$password','$fullName')";  
             $result = mysqli_query($conn, $query);
             if(!$result){
                 die("query failed !!!".mysqli_error());
